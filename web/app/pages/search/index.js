3c8294916b08ae731d23ec5431cb0ca2d4e8290a
Michal Marhan <marhanmichal@gmail.com>
2018-07-30 01:50:11 +0200

    Add search beta

--- web/app/pages/search/index.js
+++ web/app/pages/search/index.js
@@ -1,5 +1,6 @@
 import ATV from 'atvjs'
 import template from './template.hbs'
+import resultsTemplate from './resultsTemplate.hbs'
 
 import API from 'lib/prima.js'
 
@@ -20,18 +21,23 @@ const SearchPage = ATV.Page.create({
     console.log(element)
   },
   afterReady (doc) {
+    var searchField = doc.getElementById('searchField') // get the searchField element
+    var keyboard = searchField.getFeature('Keyboard') // get the keyboard of the searchField
+    keyboard.onTextChange = function () { // register listener on event onTextChange
+      makeSearch(keyboard.text) // do something with the current text
+    }
+
     const makeSearch = (text) => {
       console.log(text)
+
       let getResults = function (callback) {
         ATV.Ajax
           .get(API.url.search(text))
           .then(function (xhr) {
             // xhr succeeded
             var vysledky = xhr.response
-            console.log('Response')
-            console.log(vysledky)
-            // call resolve with the data that will be applied to the template
-            // you can even call resolve with false to skip navigation
+            // console.log('Response')
+            // console.log(vysledky)
             callback(vysledky)
           }, function (xhr) {
             // xhr failed
@@ -41,15 +47,17 @@ const SearchPage = ATV.Page.create({
           })
       }
       getResults(function (currentResponse) {
-        // ATV.Navigation.navigate(doc, ATV._.extend(currentResponse), true)
-        ATV.Navigation.navigate('search', ATV._.extend(currentResponse), true)
+        doc.getElementById('test').innerHTML = 'Něco se děje, ale ne dostatek :/'
+        // let parsed = ATV.Page.makeDom({
+        //  template: resultsTemplate,
+        //  data: currentResponse
+        // })
+        let parsed = resultsTemplate(currentResponse)
+        console.log('Ted to prijde')
+        console.log(parsed)
+        doc.getElementById('replace').innerHTML = parsed
+        // ATV.Navigation.navigate('search', ATV._.extend(currentResponse), true)
       })
-    }
-
-    var searchField = doc.getElementById('searchField') // get the searchField element
-    var keyboard = searchField.getFeature('Keyboard') // get the keyboard of the searchField
-    keyboard.onTextChange = function () { // register listener on event onTextChange
-      makeSearch(keyboard.text) // do something with the current text
     }
   }
 
